f0af5fffe0ff1120c4288e183fc19aa1e9727e48
“zzhaobraze” <Zeyuan.zhao@braze.com>
2019-12-04 12:08:08 -0500

    update SEO

--- _plugins/noindex.rb
+++ _plugins/noindex.rb
@@ -15,8 +15,9 @@ module Jekyll
       hiddenpage = Liquid::Template.parse(@hidden).render(context)
       currentpage = context.registers[:page]
       hidepage = currentpage['hidden'].nil? ? false : currentpage['hidden']
+      noindex = currentpage['noindex'].nil? ? false : currentpage['noindex']
 
-      if  (ENV['SITE_URL'].to_s.downcase != 'https://www.braze.com') || (hiddenpage.start_with? '_hidden') || (hidepage)
+      if  (ENV['SITE_URL'].to_s.downcase != 'https://www.braze.com') || (hiddenpage.start_with? '_hidden') || (hidepage) || (noindex)
         "<meta name=\"robots\" content=\"noindex, nofollow\" >"
       else
         "<meta name=\"google-site-verification\" content=\"kI0o3QRqDw5zhtd9W5umZTzLTDe6X1tp-gybtFg_7bQ\" />"
