11aa9a2be483d2f306a7ebf412aca8d290eebffb
Kellie Hawks <kellie.hawks@braze.com>
2021-09-14 11:54:34 -0400

    Collapse Key

--- _docs/_developer_guide/platform_integration_guides/fireos/content_cards.md
+++
@@ -1,7 +0,0 @@
----
-page_order: 3
-nav_title: Content Cards
-config_only: true
-layout: blank_config
-hidden: true 
----
--- assets/js/broken_redirect_list.js
+++ assets/js/broken_redirect_list.js
@@ -431,4 +431,3 @@ validurls['/docs/developer_guide/platform_integration_guides/ios/initial_sdk_set
 validurls['/docs/developer_guide/platform_integration_guides/ios/initial_sdk_setup/manual_integration_options/'] = '/docs/developer_guide/platform_integration_guides/ios/initial_sdk_setup/installation_methods/manual_integration_options/';
 
 validurls['/docs/developer_guide/platform_integration_guides/android/push_notifications/troubleshooting/#troubleshooting-scenarios'] = '/docs/developer_guide/platform_integration_guides/android/push_notifications/android/troubleshooting/#troubleshooting-scenarios';
-
