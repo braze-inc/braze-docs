185d6c0b0e82d2e45d35dc4959c5e3ee0aab3878
“zzhaobraze” <Zeyuan.zhao@braze.com>
2021-11-03 12:12:02 -0400

    update to jekyll 4

--- Gemfile
+++ Gemfile
@@ -1,9 +1,8 @@
 source "https://rubygems.org"
 ruby '~> 2.6.5'
-gem "jekyll", "~> 3.9.0"
+gem "jekyll", "~> 4.2.1"
 # This is the default theme for new Jekyll sites. You may change this to anything you like.
 gem "minima", "~> 2.0"
-gem 'sass'
 gem 'kramdown', ">= 2.3.1"
 gem 'kramdown-parser-gfm'
 gem 'rouge'
--- _plugins/jekyll_asset_pipeline.rb
+++ _plugins/jekyll_asset_pipeline.rb
@@ -2,14 +2,14 @@ require 'jekyll_asset_pipeline'
 
 module JekyllAssetPipeline
   class SassConverter < JekyllAssetPipeline::Converter
-    require 'sass'
+    require 'sassc'
 
     def self.filetype
       '.scss'
     end
 
     def convert
-      return Sass::Engine.new(@content, syntax: :scss, load_paths: [@dirname]).render
+      return SassC::Engine.new(@content, syntax: :scss, load_paths: [@dirname]).render
     end
   end
 
