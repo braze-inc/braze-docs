759fc958874210108b4a4ec977ea798f6ee8dd2c
Zeyuan Zhao <38015013+zzhaobraze@users.noreply.github.com>
2022-03-10 11:43:21 -0500

    Update developer_nav.js

--- assets/js/developer_nav.js
+++ assets/js/developer_nav.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
     }
     
     // if no platform is selected, or in the discolure menu, show all platforms
-    if ( (!dev_selected && !url_selected) || location.pathname.indexOf('disclosures') > -1 ) {
+    if ( !dev_selected && !url_selected || (location.pathname.indexOf('disclosures') > -1) || (location.pathname.indexOf('platform_wide') > -1) ) {
       platform_objects.each(function () {
         var id = $(this).attr('id');
         show(id);
