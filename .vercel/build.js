1d2c671cc9b5648f0e1f8f32bb4ac836f6f503f5
“zzhaobraze” <Zeyuan.zhao@braze.com>
2022-12-08 11:58:32 -0500

    Vercel test

--- .vercel/build.js
+++ .vercel/build.js
@@ -4,6 +4,11 @@ let branch_check = process.env.BRANCH_CHECK || '';
 let vercel_env = process.env.VERCEL_ENV || 'preview';
 let vercel_lang = process.env.LANGUAGE || 'en';
 
+console.log(branch_commit,branch_commit)
+console.log(branch_check,branch_check)
+console.log(vercel_env,vercel_env)
+console.log(vercel_lang,vercel_lang)
+
 // For develop and master branches, always build to make sure everything is updated
 if ((vercel_env == 'preview') && (branch_commit != 'develop')) {
 	// For preview, if language is english project, then don't build if it's an language branch
