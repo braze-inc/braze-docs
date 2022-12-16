dcf48620c3c7a7658bfc395f1560c8676799d47e
Kellie Hawks <kellie.hawks@braze.com>
2022-12-16 12:29:28 -0500

    kh/backlog_tickets

--- assets/js/broken_redirect_list.js
+++ assets/js/broken_redirect_list.js
@@ -569,5 +569,5 @@ validurls['/docs/developer_guide/platform_integration_guides/android/push_notifi
 validurls['/docs/api/endpoints/catalogs/catalogs/synchronous/delete_catalog/'] = '/docs/api/endpoints/catalogs/catalog_management/synchronous/delete_catalog_items_bulk/';
 validurls['/docs/api/endpoints/catalogs/catalogs/synchronous/get_list_catalogs/'] = '/docs/api/endpoints/catalogs/catalog_management/synchronous/get_list_catalogs/';
 validurls['/docs/api/endpoints/catalogs/catalogs/synchronous/post_create_catalog/'] = '/docs/api/endpoints/catalogs/catalog_management/synchronous/post_create_catalog/';
-
+validurls['/docs/api/endpoints/templates/content_blocks_templates/post_update_email_content_block'] = '/docs/api/endpoints/templates/content_blocks_templates/post_update_content_block/';
 validurls['/docs/user_guide/personalization_and_dynamic_content/catalogs/filtered_sets/'] = '/docs/user_guide/personalization_and_dynamic_content/catalogs/selections/';
