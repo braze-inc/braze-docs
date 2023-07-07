901622631e7bf38ae9017c41ee46dbb9e76e39a9
“zzhaobraze” <Zeyuan.zhao@braze.com>
2023-07-07 11:19:14 -0400

    Prevent robots from vercel site

--- proxy.rb
+++ proxy.rb
@@ -12,7 +12,6 @@ end
 set :port, 4000
 
 get '/' do
-  send_file File.join(settings.public_folder, 'robot.txt')
   if !(lang.nil?) && (lang != 'en')
     send_file File.join(settings.public_folder, "index_#{lang}.html")
   else
