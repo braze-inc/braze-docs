8e01d0cdf5c5701b82036276b800ae8569b223a9
Bre Fitzgerald <breanna.fitzgerald@braze.com>
2023-10-18 11:40:07 -0700

    Move mathjax to dedicated JS file

---
+++ assets/js/mathjax.js
@@ -0,0 +1,5 @@
+var script = document.createElement("script");
+script.type = "text/javascript";
+script.async = true;
+script.src = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML";
+document.head.appendChild(script);
