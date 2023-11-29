f0f9e3e745fcef034d567b398437008465e87bfb
Isaiah Robinson <95643215+internetisaiah@users.noreply.github.com>
2023-11-29 09:00:15 -0800

    Changing output to terminal instead of file to reduce uncessary clutter

--- scripts/deploy_text.sh
+++ scripts/deploy_text.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# TODO: create logic for if there's no PRs ready for deploy
 # TODO: write script description + rewrite all command descriptions
 # TODO: cleanup/reformat script using bash scripting best practices
 # TODO: add 'gh' and 'jq' to dev dependencies
 # TODO: create how to guide and add to confluence
+
+echo "Deploy text is generating..."
 
 # Fetch PR list
 PR_LIST=$(gh pr list --state all --limit 1000 --json mergedAt,number,title,url)
@@ -19,7 +22,12 @@ DEPLOY_INDEX=$(echo "$SORTED_PR_LIST" \
 FILTERED_PR_LIST=$(echo "$SORTED_PR_LIST" | jq --argjson DEPLOY_INDEX "$DEPLOY_INDEX" 'del(.[$DEPLOY_INDEX:])')
 
 # Output the result in Markdown format with modified title
-echo "$FILTERED_PR_LIST" \
+DEPLOY_TEXT=$(echo "$FILTERED_PR_LIST" \
   | jq -r '.[] | "- [" + (.number|tostring) + "](" + .url + ") " + (.title | gsub("BD-[^ ]+"; ""))' \
-  | tr -s ' ' | sort -t "[" -k2,2n \
-  > deploy_pr_body.md
+  | tr -s ' ' | sort -t "[" -k2,2n)
+
+cat << EOF
+Complete! Copy your deploy text below:
+
+$DEPLOY_TEXT
+EOF
