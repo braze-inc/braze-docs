1733837328b581c437c0113f26d39b6d2c32cee1
Isaiah Robinson <95643215+internetisaiah@users.noreply.github.com>
2023-11-30 13:24:07 -0800

    Updated TODOs

--- scripts/deploy_text.sh
+++ scripts/deploy_text.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # TODO: create logic for if there's no PRs ready for deploy
-# TODO: write script description + rewrite all command descriptions
+# TODO: write script description + rewrite all command comments 
 # TODO: cleanup/reformat script using bash scripting best practices
 # TODO: add 'gh' and 'jq' to dev dependencies
 # TODO: create how to guide and add to confluence
@@ -15,6 +15,8 @@ PR_LIST=$(gh pr list --state all --limit 1000 --json mergedAt,number,title,url)
 SORTED_PR_LIST=$(echo "$PR_LIST" | jq '[.[] | select(.mergedAt != null)] | sort_by(.mergedAt) | reverse')
 
 # Find the index of the most recent "Deploy -" PR
+# TODO: find better method. Try to do something like "check for last 1000 PRs in 'develop', then check
+# TODO: last 1000 PRs in 'master'. get a list of all PRs in 'develop' that are not in 'master'."
 DEPLOY_INDEX=$(echo "$SORTED_PR_LIST" \
   | jq -r 'to_entries | map(select(.value.title | startswith("Deploy -"))) | .[0].key')
 
@@ -23,7 +25,7 @@ FILTERED_PR_LIST=$(echo "$SORTED_PR_LIST" | jq --argjson DEPLOY_INDEX "$DEPLOY_I
 
 # Output the result in Markdown format with modified title
 DEPLOY_TEXT=$(echo "$FILTERED_PR_LIST" \
-  | jq -r '.[] | "- [" + (.number|tostring) + "](" + .url + ") " + (.title | gsub("BD-[^ ]+"; ""))' \
+  | jq -r '.[] | "- [#" + (.number|tostring) + "](" + .url + ") - " + (.title | gsub("BD-[^ ]+"; ""))' \
   | tr -s ' ' | sort -t "[" -k2,2n)
 
 cat << EOF
@@ -31,3 +33,4 @@ Complete! Copy your deploy text below:
 
 $DEPLOY_TEXT
 EOF
+
