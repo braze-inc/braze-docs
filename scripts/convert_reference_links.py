dfec73bb1a84a375351a7cfe5d0967c06a2e3817
internetisaiah <95643215+internetisaiah@users.noreply.github.com>
2024-07-12 16:32:43 -0700

    script now recurrsive, doesn't take arguments

--- scripts/convert_reference_links.py
+++ scripts/convert_reference_links.py
@@ -10,7 +10,12 @@
 # TODO: Make sure it also converts reference-style links for image tags as well. It currently does not.
 
 import os
-import sys
+
+# Retrieve the environment variable
+project_root = os.getenv('PROJECT_ROOT')
+
+# Verify the environment variable is passed correctly
+print("PROJECT_ROOT:", project_root)
 
 def create_link_dictionary(file_path):
     link_dict = {}
@@ -24,7 +29,7 @@ def create_link_dictionary(file_path):
     for line in lines:
         line = line.strip()
         
-        if line.startswith('[') and (line.find(']') != -1) and (('{{site.baseurl}}' in line) or ('http' in line) or ('www' in line)):
+        if line.startswith('[') and (line.find(']') != -1) and (('{{site.baseurl}}' in line) or ('http' in line) or ('www' in line) or ('{% image_buster' in line)):
             start_index = line.find('[') + 1
             end_index = line.find(']')
             key = line[start_index:end_index]
@@ -46,37 +51,22 @@ def replace_links(file_path, link_dict):
     for line in lines:
         for key in dict_keys:
             line = line.replace(f'][{key}]', f']({link_dict[key]})')
+        
         updated_lines.append(line)
 
     with open(file_path, 'w') as file:
         file.writelines(updated_lines)
 
-def process_file(file_path):
-    link_dict = create_link_dictionary(file_path)
-    replace_links(file_path, link_dict)
-
-def process_directory(directory_path):
-    for root, _, files in os.walk(directory_path):
+def process_directory(directory):
+    for root, _, files in os.walk(directory):
         for file in files:
             if file.endswith('.md'):
                 file_path = os.path.join(root, file)
-                process_file(file_path)
+                link_dict = create_link_dictionary(file_path)
+                replace_links(file_path, link_dict)
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 convert_reference_links.py [path/to/file/or/directory]")
-        sys.exit(1)
+# Define the directory to process
+directory_path = os.path.join(project_root, '_docs', '_api')
 
-    path = sys.argv[1]
-
-    if not os.path.exists(path):
-        print(f"Error: Path '{path}' does not exist.")
-        sys.exit(1)
-
-    if os.path.isfile(path):
-        process_file(path)
-    elif os.path.isdir(path):
-        process_directory(path)
-    else:
-        print(f"Error: Path '{path}' is neither a file nor a directory.")
-        sys.exit(1)
+# Process all files in the directory
+process_directory(directory_path)
