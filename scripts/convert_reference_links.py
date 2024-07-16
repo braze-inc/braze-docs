584ea23ae359dbf0ffa1d2d22b9aef21dbb5a042
internetisaiah <95643215+internetisaiah@users.noreply.github.com>
2024-07-16 10:35:29 -0700

    Cleaning up file

--- scripts/convert_reference_links.py
+++ scripts/convert_reference_links.py
@@ -10,20 +10,24 @@ import os
 import sys
 import re
 
+
+# For the current file, create a dictionary of reference links, where
+# NUM is the key and STRING is the value in [NUM]: STRING
 def create_link_dictionary(file_path):
     link_dict = {}
-    
+
     with open(file_path, 'r') as file:
         lines = file.readlines()
 
     # Reverse the lines to start from the last line
     lines.reverse()
 
-    pattern = re.compile(r'\[(.*?)\]:\s*(.*)')
+    # The pattern for reference links
+    pattern = re.compile(r'\[(.*?)]:\s*(.*)')
 
     for line in lines:
         line = line.strip()
-        
+
         match = pattern.match(line)
         if match:
             key = match.group(1)
@@ -34,6 +38,8 @@ def create_link_dictionary(file_path):
 
     return link_dict
 
+
+# Use the link dictionary to find and replace all reference links in the file.
 def replace_links(file_path, link_dict):
     with open(file_path, 'r') as file:
         lines = file.readlines()
@@ -44,29 +50,34 @@ def replace_links(file_path, link_dict):
     for line in lines:
         for key in dict_keys:
             line = line.replace(f'][{key}]', f']({link_dict[key]})')
-        
+
         updated_lines.append(line)
 
     with open(file_path, 'w') as file:
         file.writelines(updated_lines)
 
-def process_directory(directory):
-    for root, _, files in os.walk(directory):
-        for file in files:
-            if file.endswith('.md'):
-                file_path = os.path.join(root, file)
-                link_dict = create_link_dictionary(file_path)
-                replace_links(file_path, link_dict)
 
 def main(path):
+    # If 'path' == directory:
     if os.path.isdir(path):
-        process_directory(path)
+        for root, _, files in os.walk(path):
+            for file in files:
+                if file.endswith('.md'):
+                    file_path = os.path.join(root, file)
+                    link_dict = create_link_dictionary(file_path)
+                    replace_links(file_path, link_dict)
+
+    # If 'path' == Markdown file:
     elif os.path.isfile(path) and path.endswith('.md'):
         link_dict = create_link_dictionary(path)
         replace_links(path, link_dict)
+
+    # If 'path' is invalid:
     else:
-        print(f"Invalid path: {path}. Please provide a valid directory or a markdown file.")
+        print(f"Invalid path: {path}. \
+              Please provide a valid directory or a markdown file.")
+
 
 if __name__ == "__main__":
-    path = sys.argv[1]
-    main(path)
+    given_path = sys.argv[1]
+    main(given_path)
