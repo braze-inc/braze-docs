39d2b1652226194b35f6a51b18e55e5a3a41260c
internetisaiah <95643215+internetisaiah@users.noreply.github.com>
2024-11-01 16:56:08 -0700

    fixing usage comments

--- scripts/transform_reference_links.py
+++ scripts/transform_reference_links.py
@@ -6,7 +6,7 @@
 # For more information, see:
 # https://www.braze.com/docs/contributing/content_management/cross_referencing/
 # 
-# Usage: ./bdocs links -t [FILE|DIRECTORY]
+# Usage: ./bdocs tlinks [FILE|DIRECTORY]
 #
 # Options:
 #   FILE              Transform reference links in a single file.
