7bf8c0bed97e44fc6a5ce6184ba2725000436a1b
zzhaobraze <zeyuan.zhao@braze.com>
2024-12-19 14:19:44 -0500

    use md5 fallback

--- _plugins/tabs.rb
+++ _plugins/tabs.rb
@@ -1,3 +1,5 @@
+require 'digest/md5'
+
 module Tags
     class TabsBlock < Liquid::Block
       def initialize(tag_name, tabonly = 'false', tokens)
@@ -13,12 +15,15 @@ module Tags
           tabslist = '<ul class="ab-nav ab-nav-tabs ' + @tabclass + '_ul" id="' + @tabid + '_nav">' + "\n"
           if tabs.length > 0
             tabs.each_with_index do |tab, ind|
+              tabslug = tab[0].gsub(/[^0-9a-z]/i, '')
+              tabslug = Digest::MD5.hexdigest(tab[0]) if tabslug.empty?
+
               # scan returns array of results, only care about first match
-              tabslist += '    <li tabindex="0" class="coderow ' + tab[0].gsub(/[^0-9a-z]/i, '-')
+              tabslist += '    <li tabindex="0" class="coderow ' + tabslug
               if ind == 0
                 tabslist += ' active'
               end
-              tabslist += '"><a class="' + @tabclass + '" data-tab-target="' + @tabid + '" data-tab="' + tab[0].gsub(/[^0-9a-z]/i, '-') + '">' + tab[0] + '</a></li>' + "\n"
+              tabslist += '"><a class="' + @tabclass + '" data-tab-target="' + @tabid + '" data-tab="' + tabslug + '">' + tab[0] + '</a></li>' + "\n"
             end
           end
           tabslist += '</ul>'  + "\n"
@@ -48,7 +53,8 @@ module Tags
           content = indentation ? super.gsub(/^#{' |\t' * indentation}/, '') : super
           content = converter.convert(content)
           content = content.strip # Strip again to avoid "\n"
-          tabslug = @tab.gsub(/[^0-9a-z]/i, '-')
+          tabslug = @tab.gsub(/[^0-9a-z]/i, '')
+          tabslug = Digest::MD5.hexdigest(@tab) if tabslug.empty?
 
           '<div class="ab-tab-pane ' + tabslug + '_tab " data-tab="' + @tab + '">' + content + "</div>"
       end
@@ -66,15 +72,17 @@ module Tags
       def render(context)
           tabs = super.scan(/data\-sub\_tab=\"(.*?)\"/)
           tabslist = '<ul class="ab-sub_nav ab-sub_nav-sub_tabs ' + @tabclass + '_ul" id="' + @tabid + '_nav">' + "\n"
-
           if tabs.length > 0
             tabs.each_with_index do |tab, ind|
+              tabslug = tab[0].gsub(/[^0-9a-z]/i, '')
+              tabslug = Digest::MD5.hexdigest(tab[0]) if tabslug.empty?
+
               # scan returns array of results, only care about first match
-              tabslist += '    <li tabindex="0" class="coderow ' + tab[0].gsub(/[^0-9a-z]/i, '-') + '_sub_tab'
+              tabslist += '    <li tabindex="0" class="coderow ' + tabslug + '_sub_tab'
               if ind == 0
                 tabslist += ' sub_active'
               end
-              tabslist += '"><a class="' + @tabclass + '" data-sub_tab-target="' + @tabid + '" data-sub_tab="' + tab[0].gsub(/[^0-9a-z]/i, '-') + '_sub_tab">' + tab[0] + '</a></li>' + "\n"
+              tabslist += '"><a class="' + @tabclass + '" data-sub_tab-target="' + @tabid + '" data-sub_tab="' + tabslug + '_sub_tab">' + tab[0] + '</a></li>' + "\n"
             end
           end
           tabslist += '</ul>'  + "\n"
@@ -104,7 +112,8 @@ module Tags
           content = indentation ? super.gsub(/^#{' |\t' * indentation}/, '') : super
           content = converter.convert(content)
           content = content.strip # Strip again to avoid "\n"
-          tabslug = @tab.gsub(' ', '-')
+          tabslug = @tab.gsub(/[^0-9a-z]/i, '')
+          tabslug = Digest::MD5.hexdigest(@tab) if tabslug.empty?
 
           '<div class="ab-sub_tab-pane ' + tabslug + '_sub_tab " data-sub_tab="' + @tab + '">' + content + "</div>"
       end
