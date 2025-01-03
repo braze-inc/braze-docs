3e4debb7e52106891c5307743df2fa208c47fcae
internetisaiah <95643215+internetisaiah@users.noreply.github.com>
2025-01-02 17:02:56 -0800

    using alt method

--- assets/js/broken_redirect_list.js
+++ assets/js/broken_redirect_list.js
@@ -1273,7 +1273,7 @@ validurls['/docs/user_guide/getting_started/use_cases/lead_scoring'] = '/docs/us
 
 validurls['/docs/partners/channel_extensions/calendar/rokt_calendar/'] = '/docs/partners/home';
 validurls['/docs/partners/additional_channels/calendar/rokt_calendar/'] = '/docs/partners/home';
-validurls['/docs/partners/message_orchestration/additional_channels/calendar/rokt_calendar'] = '/docs/partners/home';
+// validurls['/docs/partners/message_orchestration/additional_channels/calendar/rokt_calendar'] = '/docs/partners/home';
 
 validurls['/docs/partners/message_personalization/dynamic_content/fresh_relevance'] = '/docs/partners/home';
 
