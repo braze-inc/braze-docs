bbfa8ef8efaa9276942e8e771017451cf2826e33
zzhaobraze <zeyuan.zhao@braze.com>
2025-01-24 00:36:33 -0500

    allow special chars for sdk tabs

--- _plugins/sdktabs.rb
+++ _plugins/sdktabs.rb
@@ -1,3 +1,5 @@
+require 'digest/md5'
+
 module Tags
     class SdkTabsBlock < Liquid::Block
       def initialize(tag_name, tabonly = 'false', tokens)
@@ -13,12 +15,15 @@ module Tags
           tabslist = '<ul class="sdk-ab-nav sdk-ab-nav-tabs ' + @tabclass + '_ul" id="' + @tabid + '_nav">' + "\n"
           if tabs.length > 0
             tabs.each_with_index do |tab, ind|
+              tabslug = tab[0].gsub(/[^0-9a-z]/i, '')
+              tabslug = Digest::MD5.hexdigest(tab[0]) if tabslug.empty?
+
               # scan returns array of results, only care about first match
-              tabslist += '    <li class="sdkrow ' + tab[0].gsub(' ', '-').gsub(/[^\w-]/, '')
+              tabslist += '    <li class="sdkrow ' + tabslug
               if ind == 0
                 tabslist += ' active'
               end
-              tabslist += '"><a class="' + @tabclass + '" data-sdk-tab-target="' + @tabid + '" data-sdk-tab="' + tab[0].gsub(' ', '-').gsub(/[^\w-]/, '') + '">' + tab[0] + '</a></li>' + "\n"
+              tabslist += '"><a class="' + @tabclass + '" data-sdk-tab-target="' + @tabid + '" data-sdk-tab="' + tabslug + '">' + tab[0] + '</a></li>' + "\n"
             end
           end
           tabslist += '</ul>'  + "\n"
@@ -48,7 +53,8 @@ module Tags
           content = indentation ? super.gsub(/^#{' |\t' * indentation}/, '') : super
           content = converter.convert(content)
           content = content.strip # Strip again to avoid "\n"
-          tabslug = @tab.gsub(' ', '-').gsub(/[^\w-]/, '')
+          tabslug = @tab.gsub(/[^0-9a-z]/i, '')
+          tabslug = Digest::MD5.hexdigest(@tab) if tabslug.empty?
 
           return '<div class="sdk-ab-tab-pane ' + tabslug + '_tab " data-sdk-tab="sdk-' + @tab + '">' + content + "</div>"
       end
@@ -69,12 +75,15 @@ module Tags
 
           if tabs.length > 0
             tabs.each_with_index do |tab, ind|
+              tabslug = tab[0].gsub(/[^0-9a-z]/i, '')
+              tabslug = Digest::MD5.hexdigest(tab[0]) if tabslug.empty?
+
               # scan returns array of results, only care about first match
-              tabslist += '    <li class="coderow ' + tab[0].gsub(' ', '-') + '_sub_sdk_tab'
+              tabslist += '    <li class="coderow ' + tabslug + '_sub_sdk_tab'
               if ind == 0
                 tabslist += ' sub_active'
               end
-              tabslist += '"><a class="' + @tabclass + '" data-sdk-sub_tab-target="' + @tabid + '" data-sdk-sub_tab="' + tab[0].gsub(' ', '-') + '_sub_sdk_tab">' + tab[0] + '</a></li>' + "\n"
+              tabslist += '"><a class="' + @tabclass + '" data-sdk-sub_tab-target="' + @tabid + '" data-sdk-sub_tab="' + tabslug + '_sub_sdk_tab">' + tab[0] + '</a></li>' + "\n"
             end
           end
           tabslist += '</ul>'  + "\n"
@@ -104,7 +113,8 @@ module Tags
           content = indentation ? super.gsub(/^#{' |\t' * indentation}/, '') : super
           content = converter.convert(content)
           content = content.strip # Strip again to avoid "\n"
-          tabslug = @tab.gsub(' ', '-')
+          tabslug = @tab.gsub(/[^0-9a-z]/i, '')
+          tabslug = Digest::MD5.hexdigest(@tab) if tabslug.empty?
 
           return '<div class="sdk-ab-sub_tab-pane ' + tabslug + '_sub_sdk_tab " data-sdk-sub_tab="' + @tab + '">' + content + "</div>"
       end
