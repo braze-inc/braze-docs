cc1f99ea85da6e7957dcbbf7ddb24b1f8e7eb76d
internetisaiah <95643215+internetisaiah@users.noreply.github.com>
2025-03-26 11:46:23 -0700

    tweaking redirect approach

--- assets/js/broken_redirect_list.js
+++ assets/js/broken_redirect_list.js
@@ -2337,7 +2337,6 @@ validurls['/docs/developer_guide/banner_cards/integration/'] = '/docs/developer_
 validurls['/docs/developer_guide/banner_cards/creating_banner_cards/'] = '/docs/developer_guide/banner_cards/creating_campaigns/';
 validurls['/docs/developer_guide/banner_cards/creating_placements/'] = '/docs/developer_guide/banner_cards/#placement-ids';
 
-validurls['/docs/developer_guide/push_notifications/live_notifications/?sdktab=android'] = '/docs/developer_guide/live_notifications/live_updates/';
-validurls['/docs/developer_guide/push_notifications/live_notifications/?sdktab=swift'] = '/docs/developer_guide/live_notifications/live_activities/';
+validurls['/docs/developer_guide/push_notifications/live_notifications/'] = '/docs/developer_guide/live_notifications/';
 
 // validurls['OLD'] = 'NEW';
